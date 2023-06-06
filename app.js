422fc6b500fa25e7e33992e6f4684edcb0e03375
Leon Atzori <Leonlabirinto2@gmail.com>
2023-06-06 16:58:44 +0200

    demain finir fethc

---
+++ app.js
@@ -0,0 +1,35 @@
+const card = document.querySelector('.copy')
+const input = document.querySelector('.type')
+const button = document.querySelector('.press')
+
+
+
+function noRefresh (event) {
+    event.preventDefault();
+fetch(`https://thecocktaildb.com/api/json/v1/1/filter.php?i=rum`)
+ .then((resp) => resp.json())
+ .then((data) =>{ 
+    
+ })
+    
+  }
+button.addEventListener('click',noRefresh)
+
+
+fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink')
+.then((resp) => resp.json())
+.then((data) =>{
+    data.drinks.forEach(element => {
+        card.insertAdjacentHTML('beforeend',`
+          <div class="col-lg-4">
+              <div class="card" style="margin: 10px">
+                  <img src="${element.strDrinkThumb}" class="card-img-top" alt="...">
+              <div class="card-body">
+                  <h5 class="card-title">${element.strDrink}</h5>
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+        `)
+    });
+});
+
